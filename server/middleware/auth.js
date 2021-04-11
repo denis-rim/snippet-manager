c3ece0cfeb243de32c453c8bc00b477c3fdf518c
denis-rim <denis.aurorim@gmail.com>
2021-04-11 21:04:54 +0300

    create token authentication middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+const auth = (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+
+    if (!token) return res.status(401).json({ errorMessage: "Unauthorized." });
+
+    const validatedUser = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = validatedUser.id;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({ errorMessage: "Unauthorized." });
+  }
+};
+
+module.exports = auth;
