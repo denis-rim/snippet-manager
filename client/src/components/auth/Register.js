1147ecd8ea20ebcec8955983515d55bb2da20a8c
denis-rim <denis.aurorim@gmail.com>
2021-04-13 20:55:21 +0300

    build register component

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+const Register = () => {
+  const [formEmail, setFormEmail] = useState("");
+  const [formPassword, setFormPassword] = useState("");
+  const [formPasswordVerify, setFormPasswordVerify] = useState("");
+
+  const register = async (e) => {
+    e.preventDefault();
+
+    const registerData = {
+      email: formEmail,
+      password: formPassword,
+      passwordVerify: formPasswordVerify,
+    };
+
+    await axios.post("http://localhost:5000/auth/", registerData);
+  };
+
+  return (
+    <div className="auth-form">
+      <h2>Register a new account</h2>
+      <form onSubmit={register}>
+        <label htmlFor="form-email">Email</label>
+        <input
+          id="form-email"
+          type="email"
+          value={formEmail}
+          onChange={(e) => setFormEmail(e.target.value)}
+        />
+
+        <label htmlFor="form-password">Password</label>
+        <input
+          id="form-password"
+          type="password"
+          value={formPassword}
+          onChange={(e) => setFormPassword(e.target.value)}
+        />
+
+        <label htmlFor="form-password-verify">Password</label>
+        <input
+          id="form-password-verify"
+          type="password"
+          value={formPasswordVerify}
+          onChange={(e) => setFormPasswordVerify(e.target.value)}
+        />
+
+        <button type="submit">Register</button>
+      </form>
+      <p>
+        Already have an account? <Link to="/login">Login instead</Link>
+      </p>
+    </div>
+  );
+};
+
+export default Register;
