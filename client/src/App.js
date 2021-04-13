bb3e1243ac752974cf1b11b13f9413aa37ebf509
denis-rim <denis.aurorim@gmail.com>
2021-04-13 21:44:11 +0300

    update cookie security options on client and server

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import Router from "./Router";
 import "./style/index.scss";
 import axios from "axios";
 
-// axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = true;
 
 const App = () => {
   return (
--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: "http://localhost:3000",
-    credential: true,
+    credentials: true,
   })
 );
 app.use(cookieParser());
